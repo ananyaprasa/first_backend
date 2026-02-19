6733e907e1ccc5fedee71c3355d5008c4b9503d7
Ananya Prasad <ananya.prasadjsr@gmail.com>
2026-02-19 19:40:34 +0530

    controllers

--- src/controller/video.controller.js
+++ src/controller/video.controller.js
@@ -20,11 +20,54 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid videoId")
+    }
+
+    const video = await Video.findById(videoId)
+    .populate("owner ", "username avatar")
+    if(!video){
+        throw new ApiError(404,"Video not found")
+    }
+
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, video, "video fetched successfully"));
+
+   
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "invalid videoId!!")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!video){
+        throw new ApiError(404, "video not found")
+    }
+
+    if(video.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "YOU ARE NOT AUTHORIZED TO UPDATE THIS VIDEO!!")
+
+    }
+
+    if(req.file){
+        const uploadedThumbnail = await uploadOnCloudinary(req.file.path);
+        video.thumbnail = uploadedThumbnail.url;
+    }
+    video.title = title || video.title;
+    video.description = description || video.description;
+
+    await video.save();
+
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, video, "video details updated successfully"));
 
 })
 
